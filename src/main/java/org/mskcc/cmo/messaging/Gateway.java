9da6ee2d82d8031a386c8a6d6d866c614ab6b086
laptop <laptop@lski3071.myfiosgateway.com>
2020-11-30 09:23:50 -0500

    Updated for the requested changes

--- src/main/java/org/mskcc/cmo/messaging/Gateway.java
+++ src/main/java/org/mskcc/cmo/messaging/Gateway.java
@@ -1,7 +1,9 @@
 package org.mskcc.cmo.messaging;
 
 public interface Gateway {
+	
     void publish(String topic, Object message) throws Exception;
     void subscribe(String topic, Class messageClass, MessageConsumer consumer) throws Exception;
     void shutdown() throws Exception;
+    
 }
--- src/main/java/org/mskcc/cmo/messaging/impl/NATSGatewayImpl.java
+++ src/main/java/org/mskcc/cmo/messaging/impl/NATSGatewayImpl.java
@@ -3,13 +3,14 @@ package org.mskcc.cmo.messaging.impl;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
 import org.mskcc.cmo.messaging.Gateway;
 import org.mskcc.cmo.messaging.MessageConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.google.gson.Gson;
-import io.nats.client.Connection;
 import io.nats.streaming.Message;
 import io.nats.streaming.MessageHandler;
 import io.nats.streaming.StreamingConnection;
@@ -17,10 +18,13 @@ import io.nats.streaming.Subscription;
 import io.nats.streaming.SubscriptionOptions;
 
 @Component
-public class NatsGatewayImpl implements Gateway {
+public class NATSGatewayImpl implements Gateway {
 	
 	@Value("${nats.flush_duration:5}")
 	private Integer natsFlushDuration;
+	
+	@Value("${nats.durable_name}")
+	private String durableName;
     
     @Autowired
     private StreamingConnection stanConnection;
@@ -30,14 +34,14 @@ public class NatsGatewayImpl implements Gateway {
     private boolean initialized;
 
 
-    public NatsGatewayImpl(Connection natsConnection) {
+    public NATSGatewayImpl(StreamingConnection natsConnection) {
         this.stanConnection = stanConnection;
         this.gson = new Gson();
         this.subscribers = new HashMap<>();
         this.initialized = (natsConnection != null);
     }
 
-    public NatsGatewayImpl() {
+    public NATSGatewayImpl() {
         this.gson = new Gson();
         this.subscribers = new HashMap<>();
         this.initialized = (stanConnection != null);
@@ -48,22 +52,24 @@ public class NatsGatewayImpl implements Gateway {
         if (!initialized) return;
         String msg = gson.toJson(message);
         stanConnection.publish(topic, msg.getBytes(StandardCharsets.UTF_8));
-        //natsConnection.flush(Duration.ofSeconds(natsFlushDuration));
     }
 
     @Override
     public void subscribe(String topic, Class messageClass, MessageConsumer consumer) throws Exception {
     	if (!initialized) return;
         if (!subscribers.containsKey(topic)) {
+        	final CountDownLatch doneSignal = new CountDownLatch(10);
         	Subscription sub = stanConnection.subscribe(topic, new MessageHandler() {
         		public void onMessage(Message m) {
                 	String json = new String(m.getData(), StandardCharsets.UTF_8);
                     Object message = gson.fromJson(json, messageClass);
                     consumer.onMessage(message);
+                    doneSignal.countDown();
                 }
-             }, new SubscriptionOptions.Builder().deliverAllAvailable().build());
+             }, new SubscriptionOptions.Builder().durableName(durableName).build());
 
             subscribers.put(topic, sub);
+            doneSignal.await();
         }
     }
 
