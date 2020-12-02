b54f4f78def1d2f65cfb025cfeb349c632a8df55
laptop <laptop@lski3071.myfiosgateway.com>
2020-12-02 12:29:55 -0500

    Removed any reference to shared entities

--- src/main/java/org/mskcc/cmo/messaging/Gateway.java
+++ src/main/java/org/mskcc/cmo/messaging/Gateway.java
@@ -3,9 +3,7 @@ package org.mskcc.cmo.messaging;
 public interface Gateway {
 
 	void publish(String topic, Object message) throws Exception;
-
 	void subscribe(String topic, Class messageClass, MessageConsumer messageConsumer) throws Exception;
-
 	void shutdown() throws Exception;
 
 }
--- src/main/java/org/mskcc/cmo/messaging/MessageConsumer.java
+++ src/main/java/org/mskcc/cmo/messaging/MessageConsumer.java
@@ -1,5 +1,7 @@
 package org.mskcc.cmo.messaging;
 
 public interface MessageConsumer {
+	
 	void onMessage(Object message);
+	
 }
