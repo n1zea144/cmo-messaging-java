19443f9bba312b4e5b0747108ff1456921fda19e
Benjamin Gross <benjamin.gross@gmail.com>
2020-12-02 20:12:51 -0500

    Publisher now runs in its own thread with own streaming connection (shared nats connection)

--- src/main/java/org/mskcc/cmo/messaging/Gateway.java
+++ src/main/java/org/mskcc/cmo/messaging/Gateway.java
@@ -2,6 +2,8 @@ package org.mskcc.cmo.messaging;
 
 public interface Gateway {
 
+    void initialize() throws Exception;
+
     void publish(String topic, Object message) throws Exception;
 
     void subscribe(String topic, Class messageClass, MessageConsumer messageConsumer) throws Exception;
