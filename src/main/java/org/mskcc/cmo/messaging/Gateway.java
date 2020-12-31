4d3c9a2269f80c60b84fd0c15ec533ade72d17db
Angelica Ochoa <ochoaa@mskcc.org>
2020-12-31 15:54:42 -0500

    Add isConnected() method to messaging Gateway
    Signed-off-by: Angelica Ochoa <ochoaa@mskcc.org>

--- src/main/java/org/mskcc/cmo/messaging/Gateway.java
+++ src/main/java/org/mskcc/cmo/messaging/Gateway.java
@@ -2,6 +2,7 @@ package org.mskcc.cmo.messaging;
 
 public interface Gateway {
     void connect() throws Exception;
+    boolean isConnected();
     void publish(String topic, Object message) throws Exception;
     void subscribe(String topic, Class messageClass, MessageConsumer messageConsumer) throws Exception;
     void shutdown() throws Exception;
